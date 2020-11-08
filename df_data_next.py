9b562801529796fe5e60f804c19e1ccc755c72e8
wwagner4 <wwagner4@gmail.com>
2020-11-08 18:40:07 +0100

    fixed the fixed problems for submission

--- df_data_next.py
+++ df_data_next.py
@@ -9,7 +9,6 @@ import helpers as hlp
 
 @dataclass
 class NextConfig:
-    id: str
     df_base: Callable[[], pd.DataFrame]
     numeric_predictors: List[str]
     categorical_predictors: List[str]
@@ -17,34 +16,34 @@ class NextConfig:
 
 
 configs = {
-    'L': NextConfig(id='L', df_base=hlp.read_train_fillna,
+    'L': NextConfig(df_base=hlp.read_train_fillna,
                     categorical_predictors=['cat', 'month_nr'],
                     numeric_predictors=["cnt1", "cnt_6m", "cnt5", "cnt6", "cnt_3m", "cnt3", "cnt4"]),
-    'M': NextConfig(id='M', df_base=hlp.read_train_fillna,
+    'M': NextConfig(df_base=hlp.read_train_fillna,
                     categorical_predictors=['cat', 'month_nr'],
                     numeric_predictors=["cnt1", "cnt_6m", "cnt5", "cnt6", "cnt_3m"]),
-    'S': NextConfig(id='S', df_base=hlp.read_train_fillna,
+    'S': NextConfig(df_base=hlp.read_train_fillna,
                     categorical_predictors=['cat', 'month_nr'],
                     numeric_predictors=["cnt1", "cnt_6m", "cnt5", "cnt6"]),
-    'karl': NextConfig(id='karl', df_base=hlp.read_trainx_fillna,
+    'karl': NextConfig(df_base=hlp.read_trainx_fillna,
                        categorical_predictors=['cat', 'month_nr', 'monthgroup', 'qtr'],
                        numeric_predictors=["cnt1", "cnt2", "cnt3", "cnt4", "cnt5", "cnt6", "cnt_3m", "cnt_6m",
                                            "cnt_shop1", "cnt_shop_3m", "cnt_item1", "cnt_item_3m", "year", "price", ]),
-    'karl_not_norm': NextConfig(id='karl_not_norm', df_base=hlp.read_trainx_fillna,
+    'karl_not_norm': NextConfig(df_base=hlp.read_trainx_fillna,
                                 categorical_predictors=['cat', 'month_nr', 'monthgroup', 'qtr', 'shop_id', 'year'],
                                 numeric_predictors=["cnt1", "cnt2", "cnt3", "cnt4", "cnt5", "cnt6", "cnt_3m", "cnt_6m",
                                                     "cnt_shop1", "cnt_shop_3m", "cnt_item1", "cnt_item_3m", "price", ],
                                 normalized=False),
-    'karl_not_hot': NextConfig(id='karl_not_norm', df_base=hlp.read_trainx_fillna,
+    'karl_not_hot': NextConfig(df_base=hlp.read_trainx_fillna,
                                categorical_predictors=['cat', 'monthgroup', 'qtr', 'shop_id'],
-                               numeric_predictors=['year' 'month_nr'"cnt1", "cnt2", "cnt3", "cnt4", "cnt5", "cnt6",
+                               numeric_predictors=['year', 'month_nr', "cnt1", "cnt2", "cnt3", "cnt4", "cnt5", "cnt6",
                                                    "cnt_3m", "cnt_6m",
                                                    "cnt_shop1", "cnt_shop_3m", "cnt_item1", "cnt_item_3m", "price", ],
                                normalized=False)
 }
 
 
-def _read_data(cfg: NextConfig, data_type: str) -> hlp.Trainset:
+def _read_data(config_id: str, cfg: NextConfig, data_type: str) -> hlp.Trainset:
     """
     data_type: 'train', 'train_subm', test_subm
     :returns A trainset containing the data for training and cross validation
@@ -68,13 +67,14 @@ def _read_data(cfg: NextConfig, data_type: str) -> hlp.Trainset:
     for one_hot in cfg.categorical_predictors:
         df = hlp.onehot(df, one_hot)
 
-    df_x = df.drop(['cnt'], axis=1)
-
     # Filter relevant and remove 'relevant' column
-    df.drop(columns=['relevant'], axis=1)
     df = df[df['relevant']]
+    df.drop(columns=['relevant'], axis=1)
+
+    df_x = df.drop(['cnt'], axis=1)
     predictor_names = hlp.filter_variables(list(df_x.keys()), cfg.numeric_predictors + cfg.categorical_predictors)
     df_x = df_x[predictor_names]
+
     df_y = df[['cnt']]
 
     if cfg.normalized:
@@ -84,38 +84,46 @@ def _read_data(cfg: NextConfig, data_type: str) -> hlp.Trainset:
         y_min_max_scaler = preprocessing.MinMaxScaler()
         y_scaled = pd.DataFrame(y_min_max_scaler.fit_transform(df_y), columns=df_y.columns)
 
-        return hlp.Trainset(f'next_{cfg.id}', x_scaled, y_scaled, y_min_max_scaler)
+        return hlp.Trainset(f'next_{config_id}', x_scaled, y_scaled, y_min_max_scaler)
     else:
-        return hlp.Trainset(f'next_{cfg.id}', df_x, df_y, None)
+        return hlp.Trainset(f'next_{config_id}', df_x, df_y, None)
 
 
 def read_train_data_l() -> hlp.Trainset:
-    return _read_data(configs['L'], 'train')
+    config_id = 'L'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_m() -> hlp.Trainset:
-    return _read_data(configs['M'], 'train')
+    config_id = 'M'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_s() -> hlp.Trainset:
-    return _read_data(configs['S'], 'train')
+    config_id = 'S'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_karl() -> hlp.Trainset:
-    return _read_data(configs['karl'], 'train')
+    config_id = 'karl'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_karl_not_hot() -> hlp.Trainset:
-    return _read_data(configs['karl_not_hot'], 'train')
+    config_id = 'karl_not_hot'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_karl_not_norm() -> hlp.Trainset:
-    return _read_data(configs['karl_not_norm'], 'train')
+    config_id = 'karl_not_norm'
+    return _read_data(config_id, configs[config_id], 'train')
 
 
 def read_train_data_submission() -> hlp.Trainset:
-    return _read_data(configs['karl_not_norm'], 'train_subm')
+    config_id = 'karl_not_hot'
+    return _read_data(config_id, configs[config_id], 'train_subm')
 
 
 def read_test_data_submission() -> hlp.Trainset:
-    return _read_data(configs['karl_not_norm'], 'test_subm')
+    config_id = 'karl_not_hot'
+    return _read_data(config_id, configs[config_id], 'test_subm')
--- df_data_submission.py
+++ df_data_submission.py
@@ -1,28 +1,42 @@
 import df_data_next as ndat
 import helpers as hlp
 
-df_test = hlp.read_test()
-print(df_test)
 
-ts_train = ndat.read_train_data_submission()
-ts_test = ndat.read_test_data_submission()
+def check_variables(ts_test: hlp.Trainset, ts_train: hlp.Trainset):
+    teks = set(ts_test.x.keys())
 
-print("ts_train", ts_train.x.shape, ts_train.y.shape)
-print("ts_test", ts_test.x.shape, ts_test.y.shape)
+    trks = sorted(ts_train.x.keys())
+    te_ok = []
+    te_missing = []
+    for trk in trks:
+        if trk in teks:
+            te_ok.append(trk)
+        else:
+            te_missing.append(trk)
 
-teks = set(ts_test.x.keys())
+    te_ok = sorted(te_ok)
+    te_missing = sorted(te_missing)
 
-trks = sorted(ts_train.x.keys())
-te_ok = []
-te_missing = []
-for trk in trks:
-    if trk in teks:
-        te_ok.append(trk)
-    else:
-        te_missing.append(trk)
+    print('OK:', te_ok)
+    print('MISSING:', te_missing)
 
-te_ok = sorted(te_ok)
-te_missing = sorted(te_missing)
 
-print('OK:', te_ok)
-print('MISSING:', te_missing)
+def basic_tests():
+    test = hlp.read_test()
+    print(test)
+
+    trainx = hlp.read_trainx_fillna()
+    month = trainx.groupby('month_nr').count()
+    print(month)
+
+
+def subm():
+    _ts_train = ndat.read_train_data_submission()
+    _ts_test = ndat.read_test_data_submission()
+
+    print("train:", _ts_train.id, _ts_train.x.shape, _ts_train.y.shape)
+    print("test: ", _ts_test.id, _ts_test.x.shape, _ts_test.y.shape)
+
+
+if __name__ == '__main__':
+    subm()
